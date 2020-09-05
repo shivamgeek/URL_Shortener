0fa3beec3b7cb4f1fa24f45dcd30e21b1e57ab79
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-05 22:58:23 +0530

    Added URL Entity

---
+++ src/main/java/com/shivam/Entity/URL.java
@@ -0,0 +1,81 @@
+package com.shivam.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="url_table")
+public class URL {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="full_url")
+	private String fullUrl;
+	
+	@Column(name="short_url")
+	private String shortUrl;
+	
+	@Column(name="expiration_date")
+	private String expirationDate;
+	
+	private User user;
+
+	public URL() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullUrl() {
+		return fullUrl;
+	}
+
+	public void setFullUrl(String fullUrl) {
+		this.fullUrl = fullUrl;
+	}
+
+	public String getShortUrl() {
+		return shortUrl;
+	}
+
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
+	}
+
+	public String getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(String expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "URL [id=" + id + ", fullUrl=" + fullUrl + ", shortUrl=" + shortUrl + ", expirationDate="
+				+ expirationDate + "]";
+	}
+	
+	
+}
