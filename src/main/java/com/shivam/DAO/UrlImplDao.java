a022c33cd38cec240d12215e4bb803c69d13ebc4
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-06 11:18:26 +0530

    Marked Dao implmentation as @Repository

--- src/main/java/com/shivam/DAO/UrlImplDao.java
+++ src/main/java/com/shivam/DAO/UrlImplDao.java
@@ -6,9 +6,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.shivam.Entity.URL;
 
+@Repository
 public class UrlImplDao implements UrlDao {
 
 	@Autowired
--- src/main/java/com/shivam/DAO/UserImplDao.java
+++ src/main/java/com/shivam/DAO/UserImplDao.java
@@ -6,10 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.shivam.Entity.URL;
 import com.shivam.Entity.User;
 
+@Repository
 public class UserImplDao implements UserDao {
 
 	@Autowired
