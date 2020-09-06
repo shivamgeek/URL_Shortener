dd2e1c0ad693811146d64a031920405b3e17304f
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-06 13:56:22 +0530

    Add UserController for MVC

---
+++ src/main/java/com/shivam/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.shivam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shivam.Entity.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+	
+}
