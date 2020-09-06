5f96469a3f4f0090bd61024e0d6634b26a9f2344
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-06 11:17:08 +0530

    CRUD for UserImplDao

---
+++ src/main/java/com/shivam/DAO/UserImplDao.java
@@ -0,0 +1,48 @@
+package com.shivam.DAO;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.shivam.Entity.URL;
+import com.shivam.Entity.User;
+
+public class UserImplDao implements UserDao {
+
+	@Autowired
+	SessionFactory mySessionFactory;
+	
+	public void saveUser(User u) {
+		Session session = mySessionFactory.getCurrentSession();
+		session.saveOrUpdate(u);
+	}
+
+	public void deleteUser(int id) {
+		Session session = mySessionFactory.getCurrentSession();
+		Query<User> q = session.createQuery("delete from User where id=:id",User.class);
+		q.setParameter("id", id);
+		q.executeUpdate();
+
+	}
+
+	public User getUserFromId(int id) {
+		Session session = mySessionFactory.getCurrentSession();
+		User u = session.get(User.class, id);
+		return u;
+	}
+
+	public List<User> getAllUsers() {
+		Session session = mySessionFactory.getCurrentSession();
+		List<User> list = session.createQuery("from User",User.class).list();
+		return list;
+	}
+
+	public List<URL> getUserUrls(int id) {
+		User u = getUserFromId(id);
+		return u.getUrlList();
+	}
+
+}
